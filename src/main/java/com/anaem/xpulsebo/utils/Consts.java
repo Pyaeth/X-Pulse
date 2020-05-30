fc6b92bf06ec67a0b586382753fb59f0ab05a9f0
Pyaeth <pyaeth@users.noreply.github.com>
2020-05-30 21:27:35 +0300

    logs+environment-based app properties

--- src/main/java/com/anaem/xpulsebo/utils/Consts.java
+++ src/main/java/com/anaem/xpulsebo/utils/Consts.java
@@ -3,7 +3,7 @@ package com.anaem.xpulsebo.utils;
 public class Consts {
 	private static final String password = "aembopc1";
 	private static final String className = "org.mariadb.jdbc.Driver";
-	private static final String url = "jdbc:mariadb://10.10.0.14:3306/xpulsebo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private static final String url = "jdbc:mariadb://10.10.0.3:3306/xpulsebo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static final String user = "root";
 	public static String getPassword() {
 		return password;
